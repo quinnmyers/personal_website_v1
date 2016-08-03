c215ca7ff6199a5d0be4c8788b947765b488ca8b
Quinn <quinnsmyers@gmail.com>
2016-08-02 19:05:53 -0700

    fixed nav & mobile nav, fixed scrolling

--- assets/js/header_script.js
+++ assets/js/header_script.js
@@ -5,3 +5,29 @@ $(document).ready(function () {
     $(".mobile-nav").toggleClass("is-open");
   });
 });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+//not sure why yet, but I can either have smooth scrolling or active links
+//cannot have both yet
+//$(document).ready(function () {
+//  $(".nav").on("click",function () {
+//    $('a').removeClass("is-active");
+//    $(this).find("a").addClass("is-active");
+//  });
+//});
